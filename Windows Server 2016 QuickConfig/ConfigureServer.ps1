6abafff371fda63dcb8324bf66ee8a0f021212fb
StormWind Studios <46032821+StormWindStudios@users.noreply.github.com>
2019-10-18 22:15:56 -0700

    Update ConfigureServer.ps1

--- Windows Server 2016 QuickConfig/ConfigureServer.ps1
+++ Windows Server 2016 QuickConfig/ConfigureServer.ps1
@@ -9,30 +9,46 @@
 . (Join-Path $PSScriptRoot "\Files\Variables.ps1") 
 . (Join-Path $PSScriptRoot "\Files\Functions.ps1")
 
-
+#
+# Change hostname
+#
 Write-Host `nChanging hostname -ForegroundColor Green
 Set-Hostname $HOSTNAME
 Write-Host `tDone.
 
+#
+# Change timezone
+#
 Write-Host `nSetting timezone -ForegroundColor Green
 Set-NewTimeZone $TIMEZONE
 Write-Host `tDone.
 
+#
+# Configure static IPs on nices
+#
 Write-Host `nConfiguring network. -ForegroundColor Green
 Configure-Network $IPCONFIG
 Write-Host `tDone.
 
+#
+# Switch to high-performance power plan
+#
 Write-Host `nSetting High Performance power plan -ForegroundColor Green
 Set-HighPerformancePowerPlan
 Write-Host `tDone.
 
+#
+# Disable unnecessary services
+#
 $services = Import-CsvToArray (Join-Path $PSScriptRoot "\Files\services.csv")
-
 Write-Host Disabling unnecessary services. -ForegroundColor Green
 foreach ($service in $services) {
   Disable-Service $service.Name $service.'Full Name'
   }
 
+#
+# Disable IE ESC
+#
 Write-Host Disabling IE Enhanced Security Configuration -Foregroundcolor Green
 Disable-ieESC
 Write-Host `tDone.
